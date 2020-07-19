806507d556eea3eba2306d1d66d7b278d51a770e
sjannappagaari <suresh.jannappagaari@live.com>
2020-07-19 19:38:02 +0100

    another commit comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world is my simple java program");
+		System.out.println("Hello world is my simple java program - another commit");
 	}
 
 }
